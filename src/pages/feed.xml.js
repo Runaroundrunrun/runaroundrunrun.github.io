4f40b7fee9523af7211ae01ac10cf40d79bb8a59
Runaroundrunrun <suzuki-runa@proton.me>
2026-09-05 12:47:36 +0900

    edited feed.xml.js

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog');
   return rss({
     title: 'Runa\'s Playground',
     description: 'a place where I can have fun, explore, and experiment',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md')),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      published: post.data.published,
+      link: `/posts/${post.id}/`
+    })),
     customData: `<language>en-us</language>`,
   });
 }
